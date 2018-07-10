60df18f29447f0ecca66b0217f10386e3a2a8465
Stephanie Lampotang <slampotang@fb.com>
2018-07-09 21:20:35 -0700

    the layout up to uploading pics is good but share isnt functional

---
+++ fbuinstagram/ComposeViewController.m
@@ -0,0 +1,95 @@
+//
+//  ComposeViewController.m
+//  fbuinstagram
+//
+//  Created by Stephanie Lampotang on 7/9/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+#import "Post.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *picToUpload;
+@property (weak, nonatomic) IBOutlet UITextView *captionLabel;
+@property (strong, nonatomic) Post *myNewPost;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)uploadTap:(id)sender {
+    [self choosePic];
+}
+
+
+
+- (void)choosePic {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    // editedImage = [editedImage resizeImage]
+    // Do something with the images (based on your use case)
+    self.picToUpload.image = originalImage;
+//    [self.posts addObject:editedImage];
+//    [self.tableView reloadData];
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)cancelUpload:(id)sender {
+    [self performSegueWithIdentifier:@"cancelUpload" sender:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- fbuinstagram/HomeViewController.m
+++ fbuinstagram/HomeViewController.m
@@ -21,60 +21,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    [self.tableView reloadData];
+    // FILL THE ARRAY OF POSTS
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    
-}
-
-- (IBAction)uploadTap:(id)sender {
-    [self choosePic];
-}
-
-
-
-- (void)choosePic {
-    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
-    imagePickerVC.delegate = self;
-    imagePickerVC.allowsEditing = YES;
-    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
-    }
-    else {
-        NSLog(@"Camera 🚫 available so we will use photo library instead");
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    }
-    
-    [self presentViewController:imagePickerVC animated:YES completion:nil];
-}
-
-- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
-    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
-    
-    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
-    resizeImageView.image = image;
-    
-    UIGraphicsBeginImageContext(size);
-    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return newImage;
-}
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
-    // Get the image captured by the UIImagePickerController
-    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-    // editedImage = [editedImage resizeImage]
-    // Do something with the images (based on your use case)
-    [self.posts addObject:editedImage];
+    self.tableView.rowHeight = 100;
+    // Do any additional setup after loading the view.
     [self.tableView reloadData];
-    
-    // Dismiss UIImagePickerController to go back to your original view controller
-    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"hopefully here last");
 }
 
 - (void)didReceiveMemoryWarning {
@@ -98,10 +51,10 @@
     PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"postCell"];
     //cell.delegate = self;
 
-    Post * curPost = self.posts[indexPath.row];
+    //Post * curPost = self.posts[indexPath.row];
     cell.captionLabel.text = @"ahmazing";
     //[cell setPost:curPost];
-    
+    NSLog(@"hopefully here first");
 //    cell.frame.size.height = CGRectMake(cell.frame.origin.x, cell.frame.origin.x, cell.frame.size.width, 50);
 
     return cell;
